27fbb57eb0a6defbd9320a2120c94b7d16055b14
dev-nomi <noman.khalid.freelance@gmail.com>
2022-08-12 14:34:56 +0500

    Added task components and icons.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
 import Header from './components/header'
+import Tasks from './components/tasks'
+import { useState } from 'react'
 
 function App() {
+
+  const [tasks, setTasks] =  useState([
+    {
+      "id": 1,
+      "text": "Doctors Appointment",
+      "day": "Feb 5th at 2:30pm",
+      "reminder": true
+    },
+    {
+      "id": 2,
+      "text": "Meeting at School",
+      "day": "Feb 6th at 1:30pm",
+      "reminder": true
+    }
+  ])
+
   return (
     <div className='container'>
       <Header />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
